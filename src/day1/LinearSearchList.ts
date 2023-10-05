80fdfec038607385f043b87c56f6da6c792ca47f
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-10-05 12:14:56 -0700

    test with command npx jest linear, all pass

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,12 @@
 export default function linear_search(haystack: number[], needle: number): boolean {
 
+    // Big O notation 0(n)
+
+    for ( let i = 0; i < haystack.length; i++ ) {
+        if (haystack[i] === needle){
+            return true
+        }
+    }
+
+    return false
 }

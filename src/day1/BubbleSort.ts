58213b67e38806fd0bfdbda341c4a6b2bb6256ca
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-11-04 08:33:49 -0700

    bubble pass

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,25 @@
+/**
+ * Time Complexity : O(n^2)
+ * 
+ */
 export default function bubble_sort(arr: number[]): void {
+    const len = arr.length
 
+    for (let i = 0; i < len; ++i) {
+
+        /**
+         * On the first iteration the last item will be sorted
+         * Needs to walk up to and not include the last item,
+         * each iteration we need to check if the next item is greater than the current one,
+         * if it is, then swap them
+         */
+        for (let j = 0; j < len - 1 - i; ++j) {
+
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+    }
 }

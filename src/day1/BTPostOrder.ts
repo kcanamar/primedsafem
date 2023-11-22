417746d9aead73073a8af02385ef8d0bc0c3ea76
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-11-21 18:28:12 -0800

    postorder, pass

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,20 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]):number[] {
+    // * base case
+    if (!curr) {
+        return path;
+    }
 
+    // * Recursive Steps
+    // * pre
+    // * recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+    
+    // * post
+    path.push(curr.value);
+    return path;
+
+}
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
